b1f76f1a3807c9b68fde83bccba11274932c8707
LandoAugusto <lando.augusto20@gmail.com>
2017-03-21 20:08:38 -0300

    Classes de Paginação

---
+++ Leandro.LojaVirtual.Web/Models/Paginacao.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Leandro.LojaVirtual.Web.Models
+{
+    public class Paginacao
+    {
+        public Int32 ItensTotal { get; set; }
+        public Int32 itensPorPagina { get; set; }
+        public Int32 PaginaAtual { get; set; }
+        public Int32 TotalPaginas
+        {
+            get
+            {
+                return (Int32)Math.Ceiling((decimal)ItensTotal / itensPorPagina);                    ;
+
+            }
+        }
+    }
+}
