8ec9cc50cd31cb50367d8788ac62c7297ce6ad06
LandoAugusto <lando.augusto20@gmail.com>
2017-03-30 20:47:44 -0300

    Fechar Pedido

--- Leandro.LojaVirtual.Web/Controllers/CarrinhoController.cs
+++ Leandro.LojaVirtual.Web/Controllers/CarrinhoController.cs
@@ -64,5 +64,10 @@ namespace Leandro.LojaVirtual.Web.Controllers
                 ReturnUrl = returnUrl
             });
         }
+
+        public ViewResult FecharPedido()
+        {
+            return View(new Pedido());
+        }
     }
 }
